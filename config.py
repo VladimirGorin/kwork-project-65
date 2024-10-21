59f4e9964049d2073858e8e668343a89cc0de1b1
VladimirGorin <vladimirgorin410@gmail.com>
2024-10-21 23:50:46 +0600

    add emoji

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ LOG_FILE = './log/main.log'
 MESSAGES_FILE = './data/messages.txt'
 SYNONYMS_FILE = './data/synonyms.txt'
 
-ENVELOPE_EMOJI = ["✉️", "📩", "📨", "💌", "💌"]
+ENVELOPE_EMOJI = ["✉️", "📩", "📨", "💌", "💌", "✉", "✉️", "📧", "📨", "📩", "💌", "🖂", "🖃", "💼", "📫", "📬", "📭", "📪", "📮", "📨", "✉️", "✉️"]
 
 
-ENVELOPE_TIME_BEFORE_SEND_MESSAGE = 3 # Секунды
-MAX_ENVELOPE_MESSAGES_ALL_SESSIONS = 10 # Штук
+ENVELOPE_TIME_BEFORE_SEND_MESSAGE = 3  # Секунды
+MAX_ENVELOPE_MESSAGES_ALL_SESSIONS = 10  # Штук
